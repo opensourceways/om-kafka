2f2d91dd6335958d6ca0fa50b0ab66bf2757bd92
kaede10 <sch_na@163.com>
2024-04-29 09:28:48 +0800

    add openMind model event consumer

--- src/main/java/CommonClass/Parent.java
+++ src/main/java/CommonClass/Parent.java
@@ -6,7 +6,8 @@ import org.apache.kafka.common.TopicPartition;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,25 +17,36 @@ import java.util.concurrent.Executors;
  * @date 2021/3/3 17:04
  * @description:
  */
-public class Parent extends Thread{
-    protected  Properties properties= PropertiesUtils.readProperties();
-    protected ArrayList<KafkaConsumer<String,String>>  KafkaConsumerList= new ArrayList<>();
-    protected String esIndex=properties.getProperty("es.index");
-    protected  ExecutorService thread_pool= Executors.newFixedThreadPool(Integer.parseInt(properties.get("thread_size").toString()));
+public class Parent extends Thread {
+    protected Properties properties = PropertiesUtils.readProperties();
+    protected ArrayList<KafkaConsumer<String, String>> KafkaConsumerList = new ArrayList<>();
+    protected String esIndex = properties.getProperty("es.index");
+    protected ExecutorService thread_pool = Executors
+            .newFixedThreadPool(Integer.parseInt(properties.get("thread_size").toString()));
 
     public Parent() throws IOException {
         String[] topics = properties.get("kafka.topic.name").toString().split(",");
 
-        //设置kafka offset
+        // 设置kafka offset
         String topicOffset = properties.get("kafka.topic.offset").toString();
-        //topic:offset,topic2:offset2
+        // topic:offset,topic2:offset2
         String[] topciOffsets = topicOffset.split(",");
         for (String topciOffset : topciOffsets) {
             KafkaConsumer<String, String> kafkacustomer = new KafkaConsumer<String, String>(properties);
             String[] tf = topciOffset.split(":");
-            TopicPartition topicPartition = new TopicPartition(topics[0], Integer.parseInt(tf[0]));
-            kafkacustomer.assign(Arrays.asList(topicPartition));
-            kafkacustomer.seek(topicPartition,Integer.parseInt(tf[1]));
+
+            List<TopicPartition> topicPartitions = new ArrayList<>();
+            for (String topic : topics) {
+                TopicPartition topicPartition = new TopicPartition(topic, Integer.parseInt(tf[0]));
+                topicPartitions.add(topicPartition);
+            }
+            kafkacustomer.assign(topicPartitions);
+
+            for (TopicPartition tp : topicPartitions) {
+                kafkacustomer.seek(tp, Integer.parseInt(tf[1]));
+                // 将 offset 设置为最早的数据
+                // kafkacustomer.seekToBeginning(Collections.singleton(tp));
+            }
             KafkaConsumerList.add(kafkacustomer);
         }
     }
